c4ab480fc4b8f60b4753acce1dedfdfd944d6b6a
Ben <50995624+bcalabrese15@users.noreply.github.com>
2019-07-19 10:47:52 -0400

    7/19/19
    Now outlines the coast with a black line.
    It's not perfect, since I can't look at future pixels and I believe it's because of BufferedImage.
    I'm not sure how I can get it to recognize different blue values for the ocean on different maps, so until then I think I have to put this on hold.

--- Map.java
+++ Map.java
@@ -38,7 +38,7 @@ public class Map
 		}
 	}
 	//looks for ocean(blue) pixels and sets all pixels that are not blue to green
-	public static BufferedImage process(BufferedImage img)
+	public static BufferedImage continentize(BufferedImage img)
 	{
 		BufferedImage outputImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 		//creates a new blank image that is the same size at the original which we will fill in
@@ -48,7 +48,7 @@ public class Map
 			{//height/width increment statements
 				String clr = Integer.toHexString(img.getRGB(j, i)).substring(2);//converts int RGB values of the pixel to hex
 			    Color c = Color.decode("#"+clr.toUpperCase());//converts hex to a color object which we will extract the blue value from
-				if(c.getBlue() >= 220)//adjust this for different blue values(different ocean hues
+				if(c.getBlue() >= 234)//adjust this for different blue values(different ocean hues
 				{
 					outputImage.setRGB(j, i, -3610881);//sets ocean to a more oceanic blue
 				}
@@ -60,10 +60,45 @@ public class Map
 		}
 		return outputImage;
 	}
+	public static BufferedImage coastlineMaker(BufferedImage img)
+	{
+		BufferedImage outputImage = img;//have to set it to the input image or else it'll be a black map
+		for(int i = 1; i < img.getHeight(); i++)
+		{
+			for(int j = 1; j < img.getWidth(); j++)
+			{
+				String clr = Integer.toHexString(img.getRGB(j, i)).substring(2);
+				String adjneg10 = Integer.toHexString(img.getRGB(j-1, i)).substring(2);//pixel to the left of current
+				String adj0neg1 = Integer.toHexString(img.getRGB(j, i-1)).substring(2);//pixel below the current
+				Color c = Color.decode("#"+clr.toUpperCase());
+				Color cadjneg10 = Color.decode("#"+adjneg10.toUpperCase());
+				Color cadj0neg1 = Color.decode("#"+adj0neg1.toUpperCase());
+				if(cadjneg10.getBlue() >= 234 && c.getBlue() < 150)//checks if the current pixel is a coastline pixel
+				{
+					outputImage.setRGB(j, i, 0);
+				}
+				else if(cadj0neg1.getBlue() >= 234 && c.getBlue() < 150)
+				{
+					outputImage.setRGB(j, i, 0);
+				}
+				else if(cadjneg10.getBlue() < 200 && cadjneg10.getBlue() > 0 && c.getBlue() >= 234)//checks if current pixel is an ocean pixel
+				{
+					outputImage.setRGB(j, i, 0);
+				}
+				else if(cadj0neg1.getBlue() < 200 && cadj0neg1.getBlue() > 0 && c.getBlue() >= 234)
+				{
+					outputImage.setRGB(j, i, 0);
+				}
+				
+			}
+		}
+		return outputImage;
+	}
 	public static void main(String[] args)
 	{
 		BufferedImage image = loadMap(args[0]);
-		image = process(image);
+		image = continentize(image);
+		image = coastlineMaker(image);
 		outputImage(image);
 	}
 }
